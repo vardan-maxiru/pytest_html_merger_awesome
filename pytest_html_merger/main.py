bd10897b58fa491962313ddc349bb2c476ca612e
Vardan <vardan@maxiru.com>
2023-08-15 18:10:36 +0400

    add tables

--- pytest_html_merger/main.py
+++ pytest_html_merger/main.py
@@ -25,6 +25,8 @@ def merge_html_files(in_path, out_path, title):
 
     first_file = BeautifulSoup("".join(open(paths[0])), features="html.parser")
 
+    main_table = BeautifulSoup("<table id='results-table'><tbody></tbody></table>")
+
     try:
         first_file.find("link").decompose()
     except:
@@ -46,11 +48,13 @@ def merge_html_files(in_path, out_path, title):
 
     _title = title if title else os.path.basename(out_path)
     h = first_file.find("h1")
+    case_title = h.string
     h.string = _title
     t = first_file.find("title")
     t.string = _title
 
     t = first_file.find("table", {"id": "results-table"})
+    t.id = "results-table-0"
 
     cb_types = {
         "passed": [0, ""],
@@ -76,29 +80,57 @@ def merge_html_files(in_path, out_path, title):
             dur = float(tmp[4])
 
             break
+    if cb_types['failed'][0] > 0:
+        status = 'Failed'
+    elif cb_types["passed"][0] > 0:
+        status = 'Passed'
+    elif cb_types["skipped"][0] > 0:
+        status = 'Skipped'
+    
+    main_table.find('tbody').append(f'<tr><td>{case_title}</td><td>{status}</td><td>{dur}</td></tr><tr><td colspan="3" class="case-report-result">{t}</td></tr>')
 
     for path in paths:
         if path == paths[0]:
             continue
 
         second_file = BeautifulSoup("".join(open(path)), features="html.parser")
-
-        res = second_file.find_all("tbody", {"class": "results-table-row"})
         for elm in res:
             t.append(elm)
 
         res = second_file.find_all("p")
+        current_case_duration = 0
         for p in res:
             if " tests ran" in p.text:
                 tmp = p.text.split(" ")
                 test_count += int(tmp[0])
                 dur += float(tmp[4])
+                current_case_duration += float(tmp[4])
 
                 break
-
+        
+        current_cb_types = {
+            "passed": [0, ""],
+            "skipped": [0, ""],
+            "failed": [0, ""],
+            "error": [0, ""],
+            "xfailed": [0, ""],
+            "xpassed": [0, ""],
+        }
         for cb_type in cb_types:
             tmp = get_checkbox_value(second_file, cb_type)
             cb_types[cb_type][0] += tmp[0]
+            current_cb_types[cb_type][0] = tmp[0]
+
+        if current_cb_types['failed'][0] > 0:
+            status = 'Failed'
+        elif current_cb_types["passed"][0] > 0:
+            status = 'Passed'
+        elif current_cb_types["skipped"][0] > 0:
+            status = 'Skipped'
+
+        res = second_file.find_all("talbe", {"id": "results-table"})
+
+        main_table.find('tbody').append(f'<tr><td>{case_title}</td><td>{status}</td><td>{current_case_duration}</td></tr><tr><td colspan="3" class="case-report-result">{res}</td></tr>')
 
     res = first_file.find_all("p")
     for p in res:
